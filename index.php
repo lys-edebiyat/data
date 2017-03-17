0c27ec16956015b481f2db15e578e038a9b2fb48
Burak Karakan <burak.karakan@boosmart.com>
2017-03-18 02:55:37 +0300

    Removed intelliJ folder.

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 <?php
-$pathToGo = $path = 'http://' . $_SERVER['SERVER_NAME'] . __DIR__;
-$pathToGo = str_replace($_SERVER['DOCUMENT_ROOT'], '', $pathToGo);
-$pathToGo .= '/lys-edebiyat';
+$serverBase = $path = 'http://' . $_SERVER['SERVER_NAME'] . __DIR__;
+$serverBase = str_replace($_SERVER['DOCUMENT_ROOT'], '', $serverBase);
+$pathToGo = $serverBase . '/lys-edebiyat';
+$appConfigPath = $serverBase . '/LYS-Edebiyat-AppConfig.json';
 echo "Gelmen gereken yer burası değil. Adres çubuğuna '$pathToGo' yazarsan aradığını bulursun bence.";
+echo "<br>";
+echo "Eğer AppConfig'i arıyosan ona da '$appConfigPath' adresinden ulaşabilirsin.";
